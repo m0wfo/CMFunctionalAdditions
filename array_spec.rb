d9828cb021a00c28adcd07e6bed6ac44d895aaae
Chris Mowforth <chris@mowforth.com>
2011-07-17 12:12:10 +0100

    Add test bundle

---
+++ array_spec.rb
@@ -0,0 +1,15 @@
+#
+#  ArraySpec.rb
+#  CMFunctionalAdditions
+#
+#  Created by Chris Mowforth on 17/07/2011.
+#  Copyright 2011 Limpet. All rights reserved.
+#
+
+framework 'CMFunctionalAdditions'
+
+describe "foobar" do
+    it "should do something" do
+        p "Hello, world!"
+    end
+end
