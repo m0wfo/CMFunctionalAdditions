c72ea5e75839e358727e0febfcebf89a610f35f6
Chris Mowforth <chris@mowforth.com>
2011-07-29 18:07:43 +0100

    Add demo target

---
+++ CMFunctionalDemo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CMFunctionalDemo
+//
+//  Created by Chris Mowforth on 29/07/2011.
+//  Copyright 2011 Limpet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
